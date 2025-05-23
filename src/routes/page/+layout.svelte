a20700d365db7d75619063b587ae1ead57558664
xpcn <xpcn2015@gmail.com>
2025-05-23 22:04:25 +0700

    first commit

---
+++ src/routes/page/+layout.svelte
@@ -0,0 +1,3 @@
+<a href="/page/1">ไปที่เพจ 1</a>
+<a href="/page/2">ไปที่เพจ trwe</a>
+<slot />
---
+++ src/routes/page/1/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<h2>Input</h2>
+<form>
+  <label for="username">Username:</label><br />
+  <input type="text" id="username" name="username" /><br />
+  <label for="pwd">Password:</label><br />
+  <input type="password" id="pwd" name="pwd" /><br />
+  <input type="submit" />
+</form>
